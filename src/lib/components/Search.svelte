c337b0c5b10ebe576414df4b4e8706b99f3186bf
deven <ddangi@ufl.edu>
2025-09-18 18:28:09 -0400

    Hero searchbar init

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Command } from 'bits-ui';
+</script>
+
+<Command.Root class="flex w-full items-center justify-center">
+	<Command.Input
+		class="rounded-md border border-slate-900/80 bg-black/30 px-4 py-2 text-white"
+		placeholder="Find your club..."
+	/>
+</Command.Root>
