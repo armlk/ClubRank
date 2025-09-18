9fae535fe6959721c2108916255214c60ee74684
armlk <armlk4711@gmail.com>
2025-09-18 18:27:35 -0400

    update nav bar with additional links and interactivity on search bar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -5,14 +5,32 @@
 
 <NavigationMenu.Root class="z-10 flex">
 	<NavigationMenu.List class="flex w-screen list-none items-center justify-between px-10 py-5">
-		<NavigationMenu.Item>
+		<NavigationMenu.Item class="flex items-center space-x-7">
 			<NavigationMenu.Link href="/" class="transition-all hover:brightness-75">
 				<img class="w-10" src={clubRankLogo} alt="Club Rank Logo" />
+			</NavigationMenu.Link>
+			<NavigationMenu.Link
+				href="/clubs"
+				class="text-white underline decoration-transparent transition-all hover:text-white/75 hover:decoration-purple-400"
+			>
+				Clubs
+			</NavigationMenu.Link>
+			<NavigationMenu.Link
+				href="/"
+				class="text-white underline decoration-transparent transition-all hover:text-white/75 hover:decoration-purple-400"
+			>
+				Rankings
+			</NavigationMenu.Link>
+			<NavigationMenu.Link
+				href="/"
+				class="text-white underline decoration-transparent transition-all hover:text-white/75 hover:decoration-purple-400"
+			>
+				Contact Us
 			</NavigationMenu.Link>
 		</NavigationMenu.Item>
 		<NavigationMenu.Item class="space-x-3">
 			<NavigationMenu.Link
-				class="space-x-3 rounded-md border border-gray-600 bg-purple-500/10 px-4 py-2 text-white/85 transition-all hover:cursor-pointer hover:bg-purple-800/10"
+				class="space-x-3 rounded-md border border-gray-600 bg-purple-500/10 px-4 py-2 text-white/85 transition-all duration-300 hover:cursor-pointer hover:border-gray-500 hover:bg-purple-800/10 hover:pr-20"
 				><span>🔍︎</span><span>Search</span>
 			</NavigationMenu.Link>
 			<NavigationMenu.Link
