b629818af0121c97562435ffcaab03107e897122
Guhan Sattanathan <guhansattanathan@gmail.com>
2025-10-07 19:01:17 -0400

    Create first version of club component

---
+++ src/lib/components/ClubComponent.svelte
@@ -0,0 +1,24 @@
+<script>
+
+    export let clubName;
+    import OSCLogo from "$lib/assets/osc-logo.png"
+    export let description;
+    export let category;
+</script>
+
+<div class="flex w-xl bg-black/30 p-5 rounded-md">
+    <div class="flex flex-col gap-3">
+        <p class="text-5xl text-white">
+            <!-- <img src={OSCLogo} alt="Open Source Club logo" class="m-3 h-10">  -->
+            {clubName}
+        </p>
+        <div class="bg-gray-800 p-2 rounded-md">
+            <p class="text-xl text-white m-1">{description}</p>  
+        </div>
+        <p class="text-lg text-white m-1">Category: {category}</p>   
+    </div>
+    <div class="content-center">
+        <img src={OSCLogo} alt="Open Source Club logo" class="m-3"> 
+    </div>
+        
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Search from '$lib/components/Search.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import ClubComponent from '$lib/components/ClubComponent.svelte';
 </script>
 
 <Search />
+<ClubComponent clubName="Open Source Club" description="The Open Source Club (OSC) at UF is a student organization dedicated to promoting and creating open-source software by providing a collaborative, welcoming environment for students of all skill levels to work on real-world projects" category="Academic/Computer Science"/>
 <Footer />
