17eb1d1b269bbf6d6cf3b2dfc7473f5988071afa
Guhan Sattanathan <guhansattanathan@gmail.com>
2025-10-07 22:55:24 -0400

    Modify club component to match figma sketch and clean up page route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 	import Hero from '$lib/components/Hero.svelte';
 </script>
 
-<Search />
-<ClubComponent clubName="Open Source Club" description="The Open Source Club (OSC) at UF is a student organization dedicated to promoting and creating open-source software by providing a collaborative, welcoming environment for students of all skill levels to work on real-world projects" category="Academic/Computer Science"/>
-<Footer />
 <Hero />
+<!-- <Search /> -->
+<ClubComponent clubName="Open Source Club" description="The Open Source Club (OSC) at UF is a student organization dedicated to promoting and creating open-source software by providing a collaborative, welcoming environment for students of all skill levels to work on real-world projects" category="Academic/Computer Science"/>
+
+
